b629f45fbf425bc3a147936a207775e695b31cb1
Opeyemi <opeadetola@gmail.com>
2022-04-28 21:30:40 -0400

    refactored code with async/await

--- app.js
+++ app.js
@@ -3,24 +3,12 @@ let lat;
 
 const apiKey = "4d61cf0824a05bc7526fb1790bd9e842";
 
-const getLocationWeatherData = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      //   console.log(position);
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
+async function getLocationWeatherData(lat, long) {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
 
-      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+  const response = await fetch(URL, { mode: "cors" });
+  const data = await response.json();
+  console.log(data);
+}
 
-      fetch(URL, { mode: "cors" })
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-        });
-    });
-  }
-};
-
-getLocationWeatherData();
+getLocationWeatherData(7.3775, 3.947);
