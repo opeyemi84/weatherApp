893a1708a08aa1ec1c7f4a71c1118afcfd913a39
Opeyemi <opeadetola@gmail.com>
2022-04-29 11:22:27 -0400

    Updated

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-let long;
-let lat;
-
 const apiKey = "4d61cf0824a05bc7526fb1790bd9e842";
 
-async function getLocationWeatherData(lat, long) {
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+async function getLocationWeatherData(location) {
+  const URL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&appid=4d61cf0824a05bc7526fb1790bd9e842";
 
   const response = await fetch(URL, { mode: "cors" });
   const data = await response.json();
   console.log(data);
+  const { name } = data;
+  const { feels_like, humidity, temp } = data.main;
+  const { country } = data.sys;
+  const { main, description } = data.weather[0];
+  console.log(description);
 }
 
-getLocationWeatherData(7.3775, 3.947);
+getLocationWeatherData("Lagos");
